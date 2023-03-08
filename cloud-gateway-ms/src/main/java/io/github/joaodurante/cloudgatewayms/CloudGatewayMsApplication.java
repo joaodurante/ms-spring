b8e64bfb7bd76a7270ab83af5b157a9e6f415007
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2023-03-08 18:35:43 -0300

    created ms gateway project

---
+++ cloud-gateway-ms/src/main/java/io/github/joaodurante/cloudgatewayms/CloudGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaodurante.cloudgatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayMsApplication.class, args);
+	}
+
+}
