cb9183e937e1f360587db6bd24e7aba791b6b226
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2023-03-08 19:51:31 -0300

    created card microservice project

---
+++ card-ms/src/main/java/io/github/joaodurante/cardms/CardMsApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaodurante.cardms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardMsApplication.class, args);
+	}
+
+}
