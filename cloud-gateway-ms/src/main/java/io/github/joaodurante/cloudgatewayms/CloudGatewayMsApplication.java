ff20ef7dcb70560609941898ec6e91340dc89fa5
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2023-03-08 19:37:04 -0300

    implemented ms gateway

--- cloud-gateway-ms/src/main/java/io/github/joaodurante/cloudgatewayms/CloudGatewayMsApplication.java
+++ cloud-gateway-ms/src/main/java/io/github/joaodurante/cloudgatewayms/CloudGatewayMsApplication.java
@@ -2,12 +2,24 @@ package io.github.joaodurante.cloudgatewayms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudGatewayMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayMsApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/customer/**").uri("lb://customer-ms"))
+				.build();
+	}
 }
