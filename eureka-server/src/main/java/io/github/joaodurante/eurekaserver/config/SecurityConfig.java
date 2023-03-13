8df3b3e19c2c5434b7243ab3e7ccd300debe6f95
Joao Pedro De Munno Durante <joao.durante@venturus.org.br>
2023-03-13 20:09:05 -0300

    implemented spring security in eureka server

---
+++ eureka-server/src/main/java/io/github/joaodurante/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package io.github.joaodurante.eurekaserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin()
+                .and()
+                .build();
+    }
+}
